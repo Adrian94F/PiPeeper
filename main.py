9e0aaccbaf4e7b3b48df672d81fffbcdaef25f83
Adrian94F <adik.adrian@centrumxp.pl>
2016-11-16 15:50:42 +0100

    GUI

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template, Response, request
-from camera import VideoCamera
-
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -11,26 +9,11 @@ app.config['DEBUG'] = True
 def index():
     return render_template('index.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
 @app.route('/control/<string>',methods=['GET','POST'])
 def control(string):
-	if string == 'up':
+	if string == 'revAil':
 		return 'Ok'
-	if string == 'down':
-		return 'Ok'
-	if string == 'left':
-		return 'Ok'
-	if string == 'right':
+	if string == 'revEle':
 		return 'Ok'
 	else:
 		return 'Bad command'
