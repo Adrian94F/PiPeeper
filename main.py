56bef155e4780086612b1d606f3fa3bf32f2f3ec
Adrian <209865@student.pwr.edu.pl>
2016-11-23 11:13:17 +0100

    thread for servo

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, send_from_directory
+from camera import VideoCamera
+import thread
+import smbus
+import math
+import time
+import sys
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+servos = [0.0, 0.0]
+
+def servoHandler(threadName, delay):
+	while True:
+		print "*"
+		time.sleep(1)
+
 @app.route('/')
 def index(): 
-    return render_template('index.html')
+	return render_template('index.html')
+
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+			   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+	return Response(gen(VideoCamera()),
+					mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/control/<string>',methods=['GET','POST'])
 def control(string):
-	if string == 'revAil':
+	if string == 'up':
 		return 'Ok'
-	if string == 'revEle':
+	if string == 'down':
 		return 'Ok'
-	else:
-		return 'Bad command'
+	if string == 'left':
+		return 'Ok'
+	if string == 'right':
+		return 'Ok'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, passthrough_errors=False, threaded=True)
+	try:
+		thread.start_new_thread(servoHandler, ("servoHandler", 0))
+	except:
+		print "Error: unable to start thread"
+	app.run(host='0.0.0.0', port=5000, debug=True, passthrough_errors=False, threaded=True)
