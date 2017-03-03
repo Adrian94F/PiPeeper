c4fc46df5539187eab0514669ab3931d78dcffa2
root <209865@student.pwr.edu.pl>
2017-03-03 19:56:48 +0100

    passwd xD

--- main.py
+++ main.py
@@ -36,9 +36,20 @@ def set_servo_pulse(channel, pulse):
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/')
-def index(): 
-	return render_template('login.html')
+# @app.route('/')
+# def index(): 
+# 	return render_template('index.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials. Please try again.'
+        else:
+        	return render_template('index.html')
+            # return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 @app.route('/favicon.ico')
 def favicon():
